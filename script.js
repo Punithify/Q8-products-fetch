c53497ea8aabda8b8ae296410692704d8f06694b
Punithify <punithsp9@gmail.com>
2023-09-22 19:26:08 +0530

    deesign fix

--- script.js
+++ script.js
@@ -2,22 +2,54 @@ const productList = document.getElementById('productList');
 const form = document.getElementById('searchForm');
 const searchContent = document.getElementById('searchContent');
 const alertMessage = document.getElementById('alertMessage');
+const category = document.getElementById('category');
+
+function productCard(data, divType) {
+  console.log('product');
+  data.map(
+    (product) =>
+      (divType.innerHTML += `
+      <div class="col-2 card mx-2 my-2" style="width: 12rem;">
+      <img class="card-img-top" src=${product.thumbnail} alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">${product.title}</p>
+        <p class="card-text">$${product.price}</p>
+        <p class="card-text">rating : ${product.rating}</p>
+        <p class="card-text">${product.category}</p>  
+      </div>
+    </div>
+      `)
+  );
+}
 
 async function makeRequest() {
+  //to fetcg products
   const res = await fetch('https://dummyjson.com/products');
   const data = await res.json();
+  productCard(data.products, productList);
   console.log(data);
-  data.products.map(
+
+  //to fetch categories
+  const response = await fetch('https://dummyjson.com/products/categories');
+  const categories = await response.json();
+  console.log('cat', categories);
+  categories.map(
     (product) =>
-      (productList.innerHTML += `
-    <div class="card mt-2" style="width: 18rem;">
-    <img class="card-img-top" src=${product.thumbnail} alt="Card image cap">
-    <div class="card-body">
-      <p class="card-text">${product.title}</p>
-    </div>
-  </div>
+      (category.innerHTML += `
+    <option value=${product}>${product}</option>
     `)
   );
+}
+
+async function getCategory(selectCategory) {
+  const url = selectCategory
+    ? `https://dummyjson.com/products/category/${selectCategory}`
+    : `https://dummyjson.com/products/categories`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log('category function', data.products);
+
+  return data.products;
 }
 
 form.addEventListener('submit', async (e) => {
@@ -25,7 +57,9 @@ form.addEventListener('submit', async (e) => {
   searchContent.innerHTML = '';
   alertMessage.setAttribute('hidden', true);
   const searchText = e.target.searchText.value;
-  console.log(searchText);
+  const checkPrice = e.target.checkPrice.checked;
+  const selectCategory = e.target.selectCategory.value;
+  console.log(selectCategory);
   if (searchText) {
     console.log('hello');
     const res = await fetch(
@@ -35,18 +69,25 @@ form.addEventListener('submit', async (e) => {
     if (data.products.length === 0) {
       alertMessage.removeAttribute('hidden');
       alertMessage.innerHTML = 'no matching results';
+      return;
     }
-    data.products.map(
-      (product) =>
-        (searchContent.innerHTML += `
-        <div class="card mt-2" style="width: 18rem;">
-        <img class="card-img-top" src=${product.thumbnail} alt="Card image cap">
-        <div class="card-body">
-          <p class="card-text">${product.title}</p>
-        </div>
-      </div>
-        `)
-    );
+    if (checkPrice) {
+      data.products.sort((a, b) => {
+        return a.price - b.price;
+      });
+    }
+    console.log(data.products);
+    console.log('selectCategory', selectCategory);
+    if (selectCategory != '') {
+      console.log('entry');
+      const data = await getCategory(selectCategory);
+      data.sort((a, b) => {
+        return a.price - b.price;
+      });
+      console.log('sorted', data);
+      productCard(data, searchContent);
+    }
+    productCard(data.products, searchContent);
   } else {
     alertMessage.removeAttribute('hidden');
     alertMessage.innerHTML = 'Please enter Search field';
